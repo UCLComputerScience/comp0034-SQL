ab96d86bdb8c602efc83b789ad5cd56a0156c618
Sarah Nicholson <sarah.nicholson.13@ucl.ac.uk>
2019-08-08 12:13:28 +0100

    Updated the exercises

--- IndEx1_STU_NUM.py
+++ IndEx1_STU_NUM.py
@@ -1,23 +1,34 @@
 """
-Provide Python code to create a SQLite database.
+Provide Python code to create a SQLite database and insert sample data.
 Your solution may make use of either Python with sqlite3 OR Python with sqlalchemy.
 The structure of the database must be as follows:
 
-Database name: Blog
+Database name: rain
 
-Table names: Blogger, Post
+Table names: user, forecast, city
 
-Columns for Blogger table: blogger_id (integer, Foreign Key), username (text), email (text)
+Columns for user table: user_id (integer, Primary Key), username (text), email (text)
 
-Columns for Post table: post_id (integer, Primary Key), title (text), post (text), blogger_id (integer, Foreign Key)
+Columns for city table: city_id (integer, Primary Key), city (text)
 
-Add the following records to each table:
-    [Blogger]
+Columns for forecast table: forecast_id (integer, Primary Key), city_id (integer, Foreign Key), user_id (integer, Foreign Key), datetime (text), forecast (text), commeny (text)
+
+Insert the following records into each table:
+    [user: user_id, username, email]
         1, weatherman, jo@bloggs.com
-        2, gourmand123, ieatalot@finefood.co.uk
-    [Post]
-        1, Weather, Today the weather is fine., 1
-        2, Lunch, Fish and chips with ice-cream., 2
-        3, Dinner, Green thai-curry for main with mango sticky rice for dessert., 2
-"""
+        2, itrains, itrains@alot.co.uk
+        3, sunny, sunny_grl@sunshine.co.uk
+    [city: city_id, city]
+        1, London
+        2, Manchester
+        3, Birmingham
+        4, Edinburgh
+        5, Belfast
+        6, Cardiff
+    [forecast: forecast_id, city_id, user_id, datetime, forecast, comment]
+        1, 2, 2, 2020-01-27 09:00:00, Moderate rain
+        2, 6, 1, 2020-01-27 09:00:00, Heavy rain
+        3, 1, 3, 2020-01-27 09:00:00, No rain
 
+Note: SQLite does not have a datetime data type, use text and enter dates as YYYY-MM-DD HH:MM:SS
+"""
--- IndEx1_STU_NUM.sql
+++ IndEx1_STU_NUM.sql
@@ -5,47 +5,11 @@ Right click on the file name in the Project pane and select Refactor > Rename, a
 */
 
 --1. Which employees have 'IT' in their job title? (list their EmployeeId, FirstName, LastName and Title)
-SELECT EmployeeId, FirstName, LastName, Title FROM Employee WHERE Title LIKE "IT%";
 
 --2. List the names of all Artists and the titles of their albums
-SELECT Artist.Name, Album.title FROM Artist JOIN Album ON Artist.ArtistId = Album.ArtistId ORDER BY Artist.Name;
 
 --3. Which track is features on the greatest number of times in playlists and how many times is it included? (display Trac
-SELECT Track.Name, COUNT(PlaylistTrack.TrackId) FROM Track JOIN PlaylistTrack ON Track.TrackId = PlaylistTrack.TrackId LIMIT 5;
 
 --4. Provide a list of the number of tracks by each artist
 
-
 --5. How much money has been invoiced for the artist Deep Purple? (display each line item from the invoices and the total amount)
-SELECT Artist.Name, SUM(InvoiceLine.UnitPrice*InvoiceLine.Quantity) FROM Artist
-    JOIN Album on Artist.ArtistId = Album.ArtistId
-    JOIN Track on Album.AlbumId = Track.AlbumId
-    JOIN InvoiceLine on Track.TrackId = InvoiceLine.TrackId
-WHERE Artist.Name LIKE "Deep Purple";
-
-UPDATE InvoiceLine SET Quantity=2 WHERE InvoiceLineId=96;
-UPDATE InvoiceLine SET Quantity=3 WHERE InvoiceLineId=129;
-UPDATE InvoiceLine SET Quantity=4 WHERE InvoiceLineId=130;
-
-SELECT Artist.Name, InvoiceLine.InvoiceLineId, InvoiceLine.UnitPrice, InvoiceLine.Quantity FROM Artist
-    JOIN Album on Artist.ArtistId = Album.ArtistId
-    JOIN Track on Album.AlbumId = Track.AlbumId
-    JOIN InvoiceLine on Track.TrackId = InvoiceLine.TrackId
-WHERE Artist.Name LIKE "Deep Purple";
-
-/*
-Provide SQL to create a SQLite database with the following:
-Database name: Blog
-Table names: Blogger, Post
-Columns for Blogger table: blogger_id (integer, Foreign Key), username (text), email (text)
-Columns for Post table: post_id (integer, Primary Key), title (text), post (text), blogger_id (integer, Foreign Key)
-Add the following records to each table:
-    [Blogger]
-        1, weatherman, jo@bloggs.com
-        2, gourmand123, ieatalot@finefood.co.uk
-    [Post]
-        1, Weather, Today the weather is fine., 1
-        2, Lunch, Fish and chips with ice-cream., 2
-        3, Dinner, Green thai-curry for main with mango sticky rice for dessert., 2
- */
-
