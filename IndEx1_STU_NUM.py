ce875d02e61664fb6204ab185fabd9d0ef998dd8
Sarah Nicholson <sarah.nicholson.13@ucl.ac.uk>
2019-08-08 14:33:59 +0100

    Updated the exercises

--- IndEx1_STU_NUM.py
+++ IndEx1_STU_NUM.py
@@ -7,11 +7,11 @@ Database name: rain
 
 Table names: user, forecast, city
 
-Columns for user table: user_id (integer, Primary Key), username (text), email (text)
+Columns for user table: user_id (INTEGER PRIMARY KEY), username (TEXT NOT NULL), email (TEXT NOT NULL)
 
-Columns for city table: city_id (integer, Primary Key), city (text)
+Columns for city table: city_id (INTEGER PRIMARY KEY), city (TEXT NOT NULL)
 
-Columns for forecast table: forecast_id (integer, Primary Key), city_id (integer, Foreign Key), user_id (integer, Foreign Key), datetime (text), forecast (text), commeny (text)
+Columns for forecast table: forecast_id (INTEGER PRIMARY KEY), city_id (integer, Foreign Key), user_id (integer, Foreign Key), forecast_datetime (text not null), forecast (text not null), comment (text)
 
 Insert the following records into each table:
     [user: user_id, username, email]
@@ -25,10 +25,10 @@ Insert the following records into each table:
         4, Edinburgh
         5, Belfast
         6, Cardiff
-    [forecast: forecast_id, city_id, user_id, datetime, forecast, comment]
-        1, 2, 2, 2020-01-27 09:00:00, Moderate rain
-        2, 6, 1, 2020-01-27 09:00:00, Heavy rain
-        3, 1, 3, 2020-01-27 09:00:00, No rain
+    [forecast: forecast_id, city_id, user_id, forecast_datetime, forecast, comment]
+        1, 2, 2, 2020-01-27 09:00:00, Moderate rain, It is really likely to rain today, sorry folks
+        2, 6, 1, 2020-01-27 09:00:00, Heavy rain, Don't leave home without full waterproofs today!
+        3, 1, 3, 2020-01-27 09:00:00, No rain, No umbrella required.
 
 Note: SQLite does not have a datetime data type, use text and enter dates as YYYY-MM-DD HH:MM:SS
 """
--- create_db_sqlite.py
+++ create_db_sqlite.py
@@ -10,16 +10,16 @@ c = conn.cursor()
 c.execute('''
           CREATE TABLE person
           (person_id INTEGER PRIMARY KEY,
-          first_name text NOT NULL, 
-          last_name text NOT NULL)
+          first_name TEXT NOT NULL, 
+          last_name TEXT NOT NULL)
           ''')
 
 c.execute('''
           CREATE TABLE address
           (address_id INTEGER PRIMARY KEY,
-          street_number text, 
-          street_name text,
-          postcode text NOT NULL, 
+          street_number TEXT, 
+          street_name TEXT,
+          postcode TEXT NOT NULL, 
           person_id INTEGER NOT NULL,
           FOREIGN KEY(person_id) REFERENCES person(person_id))
           ''')
@@ -33,7 +33,7 @@ c.execute(sql, values)
 # Insert the address for the person
 # If you know the rowid you can use:  c.execute("INSERT INTO address VALUES(1, 'My Road', 'SE1 9PZ', 1)")
 # Since we just inserted Jo this was the last insert so we can get the id of the last inserted row using c.lastrowid:
-sql = "INSERT INTO address ('street_number', street_name, postcode, person_id) VALUES (?, ?, ?, ?)"
+sql = "INSERT INTO address (street_number, street_name, postcode, person_id) VALUES (?, ?, ?, ?)"
 values = (12, 'My Road', 'SE1 9PZ', c.lastrowid)
 c.execute(sql, values)
 
