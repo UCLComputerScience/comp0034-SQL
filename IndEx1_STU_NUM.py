8abab17c47d680949d4c9f9a254cef739429af8f
Sarah Sanders <sarah.nicholson.13@ucl.ac.uk>
2020-01-20 13:38:18 +0000

    Update IndEx1_STU_NUM.py
    updated to allow any of the course data types

--- IndEx1_STU_NUM.py
+++ IndEx1_STU_NUM.py
@@ -1,6 +1,6 @@
 """
-Provide Python code to create a SQLite database and insert sample data.
-Your solution may make use of either Python with sqlite3 OR Python with sqlalchemy.
+Provide Python code to create either a SQLite database or a MySQL database and insert sample data.
+Your solution may make use of any appropriate Python package e.g. sqlite3, mysql-connector, sqlalchemy.
 The structure of the database must be as follows:
 
 Database name: rain
@@ -11,7 +11,9 @@ Columns for user table: user_id (INTEGER PRIMARY KEY), username (TEXT NOT NULL),
 
 Columns for city table: city_id (INTEGER PRIMARY KEY), city (TEXT NOT NULL)
 
-Columns for forecast table: forecast_id (INTEGER PRIMARY KEY), city_id (integer, Foreign Key), user_id (integer, Foreign Key), forecast_datetime (text not null), forecast (text not null), comment (text)
+Columns for forecast table: forecast_id (INTEGER PRIMARY KEY), city_id (INTEGER, Foreign Key), user_id (INTEGER, Foreign Key), forecast_datetime (TEXT NOT NULL), forecast (text not null), comment (TEXT)
+
+Note: SQLite does not have a datetime data type, use TEXT and enter dates as YYYY-MM-DD HH:MM:SS
 
 Insert the following records into each table:
     [user: user_id, username, email]
@@ -26,9 +28,8 @@ Insert the following records into each table:
         5, Belfast
         6, Cardiff
     [forecast: forecast_id, city_id, user_id, forecast_datetime, forecast, comment]
-        1, 2, 2, 2020-01-27 09:00:00, Moderate rain, It is really likely to rain today, sorry folks
-        2, 6, 1, 2020-01-27 09:00:00, Heavy rain, Don't leave home without full waterproofs today!
-        3, 1, 3, 2020-01-27 09:00:00, No rain, No umbrella required.
+        1, 2, 2, 2020-01-27 09:00:00, 'Moderate rain', 'It is really likely to rain today, sorry folks'
+        2, 6, 1, 2020-01-27 09:00:00, 'Heavy rain', 'Don't leave home without full waterproofs today!'
+        3, 1, 3, 2020-01-27 09:00:00, 'No rain', 'No umbrella required.'
 
-Note: SQLite does not have a datetime data type, use text and enter dates as YYYY-MM-DD HH:MM:SS
 """
